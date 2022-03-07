343eee5b75e3ecc732b2fd62d3626af6d849c7f0
Christerpher Hunter <djhunter67@gmail.com>
2022-03-06 20:54:03 -0500

    add a route for favicon

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ app = fastapi.FastAPI()
 def main():
 
     configure()
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run("main:app",
+                host='127.0.0.1',
+                port=8000,
+                workers=2,
+                reload=True,
+                )
 
 
 def configure():
 
     configure_templates()
     configure_routes()
+
+    favicon_path = 'favicon.ico'
+
+    @app.get('/favicon.ico')
+    def favicon():
+        return fastapi.responses.FileResponse(favicon_path)
 
 
 def configure_templates():
